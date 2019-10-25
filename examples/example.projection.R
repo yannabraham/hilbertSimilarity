d4be91929398fb4193cf52b826d08ec524ba621c
Yann Abraham <ayann@its.jnj.com>
2019-10-25 11:29:52 +0200

    use an image() to show 2D projection of Hilbert curve

--- examples/example.projection.R
+++ examples/example.projection.R
@@ -1,21 +1,22 @@
 # generate a random matrix
 ncols <- 5
-mat <- matrix(rnorm(ncols*100),ncol=ncols)
+mat <- matrix(rnorm(ncols*5000),ncol=ncols)
 dimnames(mat)[[2]] <- LETTERS[seq(ncols)]
+
 # generate 4 bins with a minimum bin size of 5
 horder <- 4
 cuts <- make.cut(mat,n=horder+1,count.lim=5)
+
 # Generate the cuts and compute the Hilbert index
 cut.mat <- do.cut(mat,cuts,type='fixed')
 hc <- do.hilbert(cut.mat,horder)
+chc <- table(hc)
+idx <- as.numeric(names(chc))
+
 # project the matrix to 2 dimensions
 proj <- hilbertProjection(hc)
-data.frame(idx=hc) %>%
-    count(idx) %>%
-    mutate(x=proj[idx,1],
-           y=proj[idx,2],
-           pc=n/sum(n)) %>%
-    ggplot(aes(x=x,y=y))+
-    geom_tile(aes(fill=pc))+
-    scale_fill_gradient(low='grey80',high='blue')+
-    theme_light()
+
+# visualize the result
+img <- matrix(0,ncol=max(proj[,2])+1,nrow = max(proj[,1])+1)
+img[proj[idx,]+1] <- chc
+image(img)
